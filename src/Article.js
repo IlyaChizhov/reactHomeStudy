e5097fd2b66c5c71970cac7cd9294a840b1902bb
Илья Чижов <ilya1991chizhov@gmail.com>
2017-08-17 23:13:21 +0300

    init commit

---
+++ src/Article.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ilyaChizhov on 17.08.17.
+ */
+
+import React from 'react'
+
+export default function Article() {
+    return (
+        <div>
+            <h3>Title</h3>
+            <section>text</section>
+        </div>
+    )
+}
